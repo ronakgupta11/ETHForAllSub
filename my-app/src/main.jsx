ce8f82d401170f93da9ddd53b67a5985be483024
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-02-16 16:15:30 +0530

    project structure

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/Root';
+import ErrorPage from './ErrorPage';
+
+
+const router = createBrowserRouter([
+  {path:"/",
+    element:<Root/>,
+  errorElement:<ErrorPage/>},
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
